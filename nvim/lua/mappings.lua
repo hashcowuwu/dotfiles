2355e87a856ae88e78fd1c1d0941e50c197fe491
hashcowuwu <3189506476@qq.com>
2024-05-11 21:15:46 +0800

    change nvim dotfiles

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,6 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("t", "<ESC>", "<CMD>quit<CR>")
 map("n", "<leader>og", "<CMD>CompetiTest receive problem<CR>", { desc = "CompetiTest receive problem"})
 map("n", "<leader>or", "<CMD>CompetiTest run<CR>", { desc = "CompetiTest run"})
 map("n", "<leader>oa", "<CMD>CompetiTest receive contest<CR>", { desc = "CompetiTest receive contest"})
