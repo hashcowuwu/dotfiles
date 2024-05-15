e38080ba5d9f38c8d2952e64c09580b42fa095c6
hashcowuwu <3189506476@qq.com>
2024-05-15 19:23:38 +0800

    更换了Markdwon预览插件

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -42,10 +42,14 @@ return {
     require'lspconfig'.gopls.setup{}
   },
   {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+        require("peek").setup()
+        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
   },
   {
 	  'xeluxee/competitest.nvim',
